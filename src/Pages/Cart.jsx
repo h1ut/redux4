aef141e968e3881f4b2cb2f983f77a5fe4f971b0
h1ut <amirosmonalievgg@gmail.com>
2023-05-20 16:47:56 +0600

    удалил ненужную хрень

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,45 +1,9 @@
-// import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import '../App.css'
 
-// const Cart = () => {
-//     const products = useSelector((state) => state.cart);
-//     const dispatch = useDispatch();
-
-//     const handleRemove = (id) => {
-
-//     }
-//   return (
-//        <div className='cartMap'>
-//         <h3>Cart</h3>
-//         <div className='cartWrapper'>
-//             {products.map((product) => {
-//                          <div key={product.id} className='cartCard'>
-
-//                          <img src={product.image} alt=""  className='cartImage'/>
-//                          <h5>{product.title}</h5>
-//                          <h5>{product.price}</h5>
-//                          <button className='btn'>remove</button>
-         
-//                      </div>
-//             })}
-//             <div>
-//             <h5>total:</h5>
-//            </div>
-
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Cart
 
 import {  useSelector, } from 'react-redux'
 import '../App.css'
-// import { dell, increment, decrement, value } from '../Redux/cartSlise';
 const Cart = () => {
     const products = useSelector((state) => state.cart);
-    // const dispatch = useDispatch()
 
 
     return (
